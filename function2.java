600ff0ec6b9924d209e168d52cfdfc2ae836b81b
sibi-7 <sibi772001@gmail.com>
2024-09-10 20:57:44 +0530

    Add : sum + if else statement

---
+++ function2.java
@@ -0,0 +1,16 @@
+public class function2
+{
+ public static void main (String args[])
+ {
+  int tam=90;
+  int eng=85;
+  int math=99;
+  int sci=99;
+  int soc=100;
+  int sum=tam+eng+math+sci+soc;
+  System.out.println("You obtained mark is :" + (sum));
+
+  if (sum>450)
+	System.out.println("your O grade Student ");	
+ }
+}
