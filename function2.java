149f98e3c1b5d529efe0dacb76d03d4512f59588
sibi-7 <sibi772001@gmail.com>
2024-09-10 21:02:42 +0530

    fix : i forget previous commit else function missed

--- function2.java
+++ function2.java
@@ -6,11 +6,18 @@ public class function2
   int eng=85;
   int math=99;
   int sci=99;
-  int soc=100;
+  int soc=35;
   int sum=tam+eng+math+sci+soc;
   System.out.println("You obtained mark is :" + (sum));
 
   if (sum>450)
-	System.out.println("your O grade Student ");	
- }
+	{
+	  System.out.println("your O grade Student ");	
+	}
+  else
+	{
+	  System.out.println("your try to Obtain O grade ");
+	}
+ 
+}
 }
