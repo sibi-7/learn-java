25b6e4867d8c1d4328b6b0b937200bd102362412
sibi-7 <sibi772001@gmail.com>
2024-09-10 21:27:47 +0530

    Add: shorthand if-else statement

---
+++ shorthand.java
@@ -0,0 +1,10 @@
+public class shorthand
+{
+ public static void main(String args[])
+ {
+  int time = 10;
+  String result;
+  result=(time<12) ? " Morning ." : " afternoon ." ;	
+  System.out.println(result);
+ }
+}
