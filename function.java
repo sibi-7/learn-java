d1caf9fe3a32c1832369f0a6c5ef80c3d2ee3e78
sibi-7 <sibi772001@gmail.com>
2024-09-10 20:48:54 +0530

    Add : if else statement

---
+++ function.java
@@ -0,0 +1,18 @@
+public class function 
+	{
+		public static void main (String args[])
+		{
+			int a=10;
+			int b=20;
+			
+			if (a<b)
+			{
+				System.out.println("the given a is lesser then b :" + (a));
+			}
+			else
+			{
+				System.out.println("The given b is greater then a :" + (b));
+			}
+		}
+	}
+				
